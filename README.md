971dc2ec6acbfd25ff18f3a8d5c93fd0ae8acd42
zhangyuan <nevermakeyoucrytoli@gmail.com>
2019-12-03 15:57:29 +0800

    add myip feature

--- README.md
+++ README.md
@@ -1,5 +1,25 @@
 # ip查询工具
 
+## 仓库地址
+
+[utools_ip](https://github.com/Demired/utools_ip)
+
+## 接口来源
+
+ipip.net
+
+## 接口
+
+```sh
+https://myip.ipip.net/json
+
+https://freeapi.ipip.net
+```
+
 ## 使用方法
 
-ip 123.123.123.123
+myip
+
+or
+
+ip xxx.xxx.xxx.xxx
--- preload.js
+++ preload.js
@@ -5,7 +5,49 @@ const { shell } = require('electron');
 
 var ipip_url = "";
 window.exports = {
-    "ip_query":{
+   "get_myip":{
+      mode: 'list',
+      args: {
+         enter: (action, callbackSetList) => {
+            var url = "https://myip.ipip.net/json";
+            var title = "";
+            var desc = "";
+            request({
+               url: url,
+               method: "GET",
+               json: true,
+               headers: {
+                  "user-agent": "utools_ip 0.1 auther zhangyuan",
+                  "content-type": "application/json",
+               },
+            }, function (error, response, body) {
+               if (!error && response.statusCode == 200) {
+                  if(body.ret="ok"){
+                     desc = `${body.data.ip} 查询结果来自 ipip.net`;
+                     ipip_url = `https://www.ipip.net/ip/${body.data.ip}.html`
+                  }
+                  body.data.location.forEach(element => {
+                     title += element+" "
+                  });
+               }
+               callbackSetList([
+                  {
+                     title: title,
+                     description: desc,
+                     icon:"/ipip_logo.png",
+                  }
+               ])
+            })
+         },
+         select: (action, itemData) => {
+            window.utools.hideMainWindow()
+            shell.openExternal(ipip_url).then(() => {
+              window.utools.outPlugin()
+            })
+         },
+      }
+   },
+   "ip_query":{
       mode: 'list',
       args: {
          enter: (action, callbackSetList) => {
@@ -22,7 +64,7 @@ window.exports = {
                return
             }
             var title = "";
-            var desc = `${searchWord} 来自 ipip.net`;
+            var desc = `${searchWord} 查询结果来自 ipip.net`;
             var url = `https://freeapi.ipip.net/${searchWord}`;
             ipip_url = `https://www.ipip.net/ip/${searchWord}.html`
             request({
